2f47f7ca5627b86bf5791e8c603498ccb251bb43
Pawel Borkar <pawel@duck.com>
2023-12-19 01:05:31 +0530

    add: github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,13 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
+import adapter from '@sveltejs/adapter-static';
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
+  // for more information about preprocessors kit: {
+  adapter: adapter(),
+  prerender: {
+    enabled: true,
+    crawl: true,
+    pages: ['*'],
+  },
   preprocess: vitePreprocess(),
 }
